620fc29a572af07ae2bb111b1ff590fa5c01d6d4
Stephan Mestach <stephan.mestach@8thcolor.com>
2013-10-01 15:08:07 +0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vendored::Application.config.secret_token = 'b9f54fa648c5760e36d9564e03c589b7f4b9fd589bbe3b9ea612e0d22544522dd6c59c63e955025bb601a4ab1db96bfc2c4541af79340614b29deabfb9c6b166'
